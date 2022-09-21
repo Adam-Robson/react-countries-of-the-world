996fb5d824ea9a8ae888e1bf5a897b5200895ff9
Adam Robson <adamray312@gmail.com>
2022-09-21 08:59:15 -0700

    'setup header and footer; display flag'

---
+++ src/Components/Layout/Footer/Footer.js
@@ -0,0 +1,4 @@
+import './Footer.css';
+export default function Footer() {
+  return <footer>2022 Student Developer Alchemy Code Lab</footer>;
+}
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,15 @@
+import Showcase from '../Showcase/Showcase';
+
+export default function Main() {
+  return (
+    <main className="main-container">
+      <img
+        src="https://flagcdn.com/16x12/za.png"
+        srcSet="https://flagcdn.com/32x24/za.png 2x,
+        https://flagcdn.com/48x36/za.png 3x"
+        width="16"
+        height="12"
+        alt="South Africa"></img>
+    </main>
+  );
+}
