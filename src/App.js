2178428df2eba6cb9a334528a450ab479a126fac
Adam Robson <adamray312@gmail.com>
2022-09-21 22:25:59 -0700

    move fetch to component, appjs passes state down

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import './Components/Main/Main';
 
+import { useState, useEffect } from 'react';
 import './App.css';
-
-import Main from './Components/Main/Main';
-import Footer from './Components/Layout/Footer/Footer';
-import Header from './Components/Layout/Header/Header';
+import { getCountries } from './services/getCountries';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [continents, setContinents] = useState([]);
+  const [continent, setContinent] = useState([]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const response = await getCountries();
+      setCountries(response);
+    };
+    fetchCountries();
+  }, []);
+
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <>
+      <div className="App">
+        <h2>Countries of Earth</h2>
+        <select className="dropdown" onChange={(event) => setContinents(event.target.value)}>
+          <option value="all">all</option>
+          {!!continents.length &&
+            continents.map((cont) => (
+              <option key={cont} value={cont}>
+                {cont}
+              </option>
+            ))}
+        </select>
+      </div>
+      <div className="allCountries"></div>
+    </>
   );
 }
 
--- src/Components/Load/Load.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import './Load.css';
-
-export default function Load() {
-  return <div className="lds-heart"><div></div></div>;
-}
