d8b3e1d393e374b6ff743c4cea112f08b3496004
Adam Robson <adamray312@gmail.com>
2022-09-21 22:56:45 -0700

    finish iterating through properties of indices

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from 'react';
 import './App.css';
 import { getCountries } from './services/getCountries';
@@ -6,15 +5,26 @@ import { getCountries } from './services/getCountries';
 function App() {
   const [countries, setCountries] = useState([]);
   const [continents, setContinents] = useState([]);
+  // eslint-disable-next-line no-unused-vars
   const [continent, setContinent] = useState([]);
 
   useEffect(() => {
     const fetchCountries = async () => {
       const response = await getCountries();
       setCountries(response);
+      const continentsEach = [...new Set(response.map((cont) => cont.continent))];
+      setContinents(continentsEach);
     };
     fetchCountries();
   }, []);
+
+  const countriesFilter = () => {
+    const countriesFilterii = countries.filter(
+      (cont) =>
+        (continent === 'all' ? true : cont.continent === continent) && cont.name.toLowerCase()
+    );
+    return countriesFilterii;
+  };
 
   return (
     <>
@@ -30,7 +40,19 @@ function App() {
             ))}
         </select>
       </div>
-      <div className="allCountries"></div>
+      <div className="allCountries">
+        {countriesFilter().map((cont) => (
+          <div className="country" key={cont.name}>
+            <h3>{cont.name}</h3>
+            <img
+              src={`https://flagcdn.com/72x54/${cont.iso2.toLowerCase()}.png`}
+              width="60"
+              height="45"
+              alt={cont.name}
+            />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
